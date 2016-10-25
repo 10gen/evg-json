c02550143994b4b2a4577571d5c9bff65124196a
Shraya Ramani <shraya@mongodb.com>
2016-10-25 16:25:55 -0400

    EVG-1227 rewrite the get latest versions to support getting relative versions

--- version.go
+++ version.go
@@ -7,6 +7,7 @@ import (
 	"github.com/evergreen-ci/evergreen/db"
 	"github.com/evergreen-ci/evergreen/model/version"
 	"github.com/evergreen-ci/evergreen/plugin"
+	"github.com/evergreen-ci/evergreen/util"
 	"github.com/gorilla/mux"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
@@ -23,9 +24,32 @@ type CommitInfo struct {
 	VersionId  string    `json:"version_id"`
 }
 
+// TaskForVersionId contains the id information about the group of tasks
+// including the version id and the revision order number.
+type TaskForVersionId struct {
+	VersionId           string `bson:"vid"`
+	RevisionOrderNumber int    `bson:"r"`
+}
+
+// TaskInfo contains the relevant TaskJSON information the tasks associated with a version.
+type TaskInfo struct {
+	TaskId   string      `bson:"t_id" json:"task_id"`
+	TaskName string      `bson:"tn" json:"task_name"`
+	Variant  string      `bson:"var" json:"variant"`
+	Data     interface{} `bson:"d" json:"data"`
+}
+
+// TasksForVersion is a list of tasks that are associated with a given version id.
+type TasksForVersion struct {
+	Id    TaskForVersionId `bson:"_id" json:"id"`
+	Tasks []TaskInfo       `bson:"t" json:"tasks"`
+}
+
+// VersionData includes the TaskInfo, and Commit information for a given version.
 type VersionData struct {
-	JSONTasks []TaskJSON `json:"json_tasks"`
-	Commit    CommitInfo `json:"commit_info"`
+	JSONTasks    []TaskInfo `json:"json_tasks"`
+	Commit       CommitInfo `json:"commit_info"`
+	LastRevision bool       `json:"last_revision"`
 }
 
 // getVersion returns a StatusOK if the route is hit
@@ -37,7 +61,7 @@ func getVersion(w http.ResponseWriter, r *http.Request) {
 func findTasksForVersion(versionId, name string) ([]TaskJSON, error) {
 	var jsonForTasks []TaskJSON
 	err := db.FindAllQ(collection, db.Query(bson.M{VersionIdKey: versionId,
-		NameKey: name}), &jsonForTasks)
+		NameKey: name}).WithFields(DataKey, TaskIdKey, TaskNameKey), &jsonForTasks)
 	if err != nil {
 		if err != mgo.ErrNotFound {
 			return nil, err
@@ -67,32 +91,75 @@ func getTasksForVersion(w http.ResponseWriter, r *http.Request) {
 func getTasksForLatestVersion(w http.ResponseWriter, r *http.Request) {
 	project := mux.Vars(r)["project_id"]
 	name := mux.Vars(r)["name"]
-	var jsonTask TaskJSON
-	err := db.FindOneQ(collection, db.Query(bson.M{NameKey: name,
-		ProjectIdKey: project}).Sort([]string{"-" + RevisionOrderNumberKey}).WithFields(VersionIdKey), &jsonTask)
+	skip, err := util.GetIntValue(r, "skip", 0)
 	if err != nil {
-		if err != mgo.ErrNotFound {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		http.Error(w, "{}", http.StatusNotFound)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	if jsonTask.VersionId == "" {
-		http.Error(w, "{}", http.StatusNotFound)
-	}
-	jsonTasks, err := findTasksForVersion(jsonTask.VersionId, name)
-	if jsonTasks == nil {
-		http.Error(w, "{}", http.StatusNotFound)
+	if skip < 0 {
+		http.Error(w, "negative skip", http.StatusBadRequest)
 		return
 	}
+	pipeline := []bson.M{
+		// match on name and project
+		{"$match": bson.M{
+			NameKey:      name,
+			ProjectIdKey: project,
+		}},
+
+		// sort on the revision number
+		{"$sort": bson.M{
+			RevisionOrderNumberKey: -1,
+		}},
+
+		// group by version id
+		{"$group": bson.M{
+			"_id": bson.M{
+				"r":   "$" + RevisionOrderNumberKey,
+				"vid": "$" + VersionIdKey,
+			},
+			"t": bson.M{
+				"$push": bson.M{
+					"d":    "$" + DataKey,
+					"t_id": "$" + TaskIdKey,
+					"tn":   "$" + TaskNameKey,
+					"var":  "$" + VariantKey,
+				},
+			},
+		}},
+
+		// sort on the _id
+		{"$sort": bson.M{
+			"_id.r": -1,
+		}},
+
+		{"$skip": skip},
+		{"$limit": 2},
+	}
+
+	tasksForVersions := []TasksForVersion{}
+	err = db.Aggregate(collection, pipeline, &tasksForVersions)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
+	if len(tasksForVersions) == 0 {
+		http.Error(w, "no tasks found", http.StatusNotFound)
+		return
+	}
+
+	// we default have another revision
+	lastRevision := false
+	currentVersion := tasksForVersions[0]
+
+	// if there is only one version, then we are at the last revision.
+	if len(tasksForVersions) < 2 {
+		lastRevision = true
+	}
+
 	// get the version commit info
-	v, err := version.FindOne(version.ById(jsonTask.VersionId))
+	v, err := version.FindOne(version.ById(currentVersion.Id.VersionId))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -107,9 +174,9 @@ func getTasksForLatestVersion(w http.ResponseWriter, r *http.Request) {
 		Message:    v.Message,
 		CreateTime: v.CreateTime,
 		Revision:   v.Revision,
-		VersionId:  jsonTask.VersionId,
+		VersionId:  v.Id,
 	}
 
-	data := VersionData{jsonTasks, commitInfo}
+	data := VersionData{currentVersion.Tasks, commitInfo, lastRevision}
 	plugin.WriteJSON(w, http.StatusOK, data)
 }
